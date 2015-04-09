9c7b2f166541d393accc04fc76408153a5dc7428
Jack Pearkes <jackpearkes@gmail.com>
2015-04-09 06:55:25 -0700

    initial commit

---
+++ example.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "allow_all" {
+  name = "allow_all"
+  description = "Allow all inbound traffic"
+
+  ingress {
+      from_port = 0
+      to_port = 65535
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "web" {
+    instance_type = "t2.micro"
+
+    # Trusty 14.04
+    ami = "ami-10b68a78"
+
+    security_groups = ["${aws_security_group.allow_all.name}"]
+
+    # This will create 1 instances
+    count = 1
+
+    # Ensures that we have
+    lifecycle = {
+      create_before_destroy = true
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------
+# AWS settings
+#--------------------------------------------------------------
+variable "access_key" {
+    description = "Please enter your AWS access key"
+}
+
+variable "secret_key" {
+    description = "Please enter your AWS secret key"
+}
