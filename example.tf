f200083c6855ad83b869ec84a1e9fedd1cded8e5
Kevin Fishner <KFishner@gmail.com>
2015-05-13 17:06:23 -0700

    Update example.tf

--- example.tf
+++ example.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "main" {
     ami = "ami-2a734c42"
 
     # This will create 1 instances
-    count = 1
+    count = 2
 
     subnet_id = "${aws_subnet.main.id}"
     security_groups = ["${aws_security_group.allow_all.id}"]
